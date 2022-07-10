24731b684bece91a919d3fd1a8b7e583b826de1e
Rouhish Ray <rouhish.bkp15@gmail.com>
2022-07-11 00:28:20 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">PG in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Kolkata">PG in Kolkata</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2022 Copyright PGLife by Rouhish Ray</div>
+        <div class="footer-copyright"> Mobile: +91 8282801422</div>
+        <div class="footer-copyright">
+        <p>
+         "If this Website is beneficial for your website or business, please kindly
+         <a rel="nofollow" href="https://www.facebook.com/profile.php?id=100023372730367" target="_blank">'Ping me'</a>
+         . Please also tell your friends about our great website. Thank you."</p>
+    </div>
+    <div class="footer-box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
